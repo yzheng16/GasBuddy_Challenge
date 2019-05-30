e29189e2361ddd231f172dd7e8f8d15350752739
Yi Zheng <yzheng16@studentmail.nait.ca>
2019-05-30 11:46:35 -0600

    init project

--- GasBuddy_Challenge/AppDelegate.swift
+++ GasBuddy_Challenge/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow()
+        let layout = UICollectionViewFlowLayout()
+        window?.rootViewController = MainController(collectionViewLayout: layout)
         return true
     }
 
---
+++ GasBuddy_Challenge/MainController.swift
@@ -0,0 +1,16 @@
+//
+//  MainController.swift
+//  GasBuddy_Challenge
+//
+//  Created by Yi Zheng on 2019-05-30.
+//  Copyright © 2019 Yi Zheng. All rights reserved.
+//
+
+import UIKit
+
+class MainController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView?.backgroundColor = .red
+    }
+}
