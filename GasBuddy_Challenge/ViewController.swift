cca68fdb2a4f99dacbcb7fb858c96ac4ca531fec
Yi Zheng <yzheng16@studentmail.nait.ca>
2019-05-30 11:35:09 -0600

    Initial Commit

---
+++ GasBuddy_Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GasBuddy_Challenge
+//
+//  Created by Yi Zheng on 2019-05-30.
+//  Copyright © 2019 Yi Zheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
