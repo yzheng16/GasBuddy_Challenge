b9fed74088ce9fa1a6b7a83ef7dbc9f6c5decaf1
Yi Zheng <yzheng16@studentmail.nait.ca>
2019-05-31 10:54:28 -0600

    finished MainPage

--- GasBuddy_Challenge/HomeCell.swift
+++ GasBuddy_Challenge/HomeCell.swift
@@ -9,28 +9,42 @@
 import UIKit
 
 class HomeCell: UICollectionViewCell {
+    
+    var photo: Photo? {
+        didSet {
+            guard let label = photo?.title else{return}
+            guard let photoUrl = photo?.thumbnailImageName else{return}
+            navLabel.text = label
+            navPhoto.loadImage(urlString: photoUrl)
+            
+        }
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
+        backgroundColor = .white
         
         addSubview(navPhoto)
         addSubview(navLabel)
         addSubview(navArrow)
         
         navPhoto.anchor(topAnchor, left: leftAnchor, bottom: bottomAnchor, right: nil, topConstant: 22, leftConstant: 8, bottomConstant: 22, rightConstant: 0, widthConstant: 56, heightConstant: 56)
-        navLabel.anchor(topAnchor, left: navPhoto.rightAnchor, bottom: bottomAnchor, right: nil, topConstant: 0, leftConstant: 8, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+        
         navArrow.anchor(topAnchor, left: nil, bottom: bottomAnchor, right: rightAnchor, topConstant: 40, leftConstant: 0, bottomConstant: 40, rightConstant: 8, widthConstant: 20, heightConstant: 20)
+        navLabel.anchor(topAnchor, left: navPhoto.rightAnchor, bottom: bottomAnchor, right: navArrow.leftAnchor, topConstant: 0, leftConstant: 8, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
     }
     
-    let navPhoto: UIImageView = {
-        let iv = UIImageView()
-        iv.contentMode = .scaleAspectFill
-        iv.backgroundColor = .red
-        return iv
+    let navPhoto: CustomImageView = {
+        let civ = CustomImageView()
+        civ.contentMode = .scaleAspectFill
+        civ.clipsToBounds = true
+        return civ
     }()
     
     let navLabel: UILabel = {
         let l = UILabel()
-        l.textAlignment = .center
+        l.textAlignment = .left
         l.text = "Test"
         return l
     }()
--- GasBuddy_Challenge/MainController.swift
+++ GasBuddy_Challenge/MainController.swift
@@ -11,14 +11,46 @@ import UIKit
 class MainController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
     let cellId = "cellId"
+    var photos: [Photo]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView?.backgroundColor = .white
+        collectionView?.backgroundColor = UIColor(r: 238, g: 238, b: 244)
         
         navigationItem.title = "Photos"
         
         collectionView.register(HomeCell.self, forCellWithReuseIdentifier: cellId)
+        fetchPhotos()
+    }
+    
+    func fetchPhotos() {
+        let url = URL(string: "https://s3-us-west-2.amazonaws.com/youtubeassets/home.json")
+        URLSession.shared.dataTask(with: url!) { (data, response, err) in
+            if err != nil {
+                print(err ?? "error")
+                return
+            }
+            
+            do{
+                let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers)
+                
+                self.photos = [Photo]()
+                
+                for dictionary in json as! [[String: AnyObject]] {
+                    let photo = Photo()
+                    photo.title = dictionary["title"] as? String
+                    photo.thumbnailImageName = dictionary["thumbnail_image_name"] as? String
+                    self.photos?.append(photo)
+                }
+                DispatchQueue.main.async {
+                    self.collectionView.reloadData()
+                }
+            }catch let jsonError {
+                print(jsonError)
+            }
+            
+            
+        }.resume()
     }
     
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
@@ -26,12 +58,12 @@ class MainController: UICollectionViewController, UICollectionViewDelegateFlowLa
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
+        return photos?.count ?? 0
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! HomeCell
-        cell.backgroundColor = .yellow
+        cell.photo = self.photos![indexPath.item]
         return cell
     }
     
