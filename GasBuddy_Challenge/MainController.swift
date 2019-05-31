d496f1dca1c85658ea3df404bdf05c539e46c127
Yi Zheng <yzheng16@studentmail.nait.ca>
2019-05-31 15:45:24 -0600

    added photos filter

--- GasBuddy_Challenge/MainController.swift
+++ GasBuddy_Challenge/MainController.swift
@@ -8,19 +8,25 @@
 
 import UIKit
 
-class MainController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+class MainController: UICollectionViewController, UICollectionViewDelegateFlowLayout, UISearchBarDelegate {
     
     let cellId = "cellId"
     var photos: [Photo]?
+    var filterPhotos = [Photo]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView?.backgroundColor = UIColor(r: 238, g: 238, b: 244)
         
-        navigationItem.title = "Photos"
+//        navigationItem.title = "Photos"
         
         collectionView.register(HomeCell.self, forCellWithReuseIdentifier: cellId)
         fetchPhotos()
+        
+        let navBar = navigationController?.navigationBar
+        navBar?.addSubview(searchBar)
+        searchBar.anchor(navBar?.topAnchor, left: navBar?.leftAnchor, bottom: navBar?.bottomAnchor, right: navBar?.rightAnchor, topConstant: 0, leftConstant: 8, bottomConstant: 0, rightConstant: 8, widthConstant: 0, heightConstant: 0)
+        collectionView?.keyboardDismissMode = .onDrag
     }
     
     func fetchPhotos() {
@@ -48,6 +54,7 @@ class MainController: UICollectionViewController, UICollectionViewDelegateFlowLa
                     photo.channel = channel
                     self.photos?.append(photo)
                 }
+                self.filterPhotos = self.photos!
                 DispatchQueue.main.async {
                     self.collectionView.reloadData()
                 }
@@ -59,17 +66,43 @@ class MainController: UICollectionViewController, UICollectionViewDelegateFlowLa
         }.resume()
     }
     
+    lazy var searchBar: UISearchBar = {
+        let sb = UISearchBar()
+        sb.placeholder = "Enter Photo Name"
+        UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).backgroundColor = UIColor(r: 240, g: 240, b: 240)
+        sb.delegate = self
+        return sb
+    }()
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.isEmpty {
+            filterPhotos = photos!
+        }else{
+            filterPhotos = photos!.filter({ (photo) -> Bool in
+                return photo.title!.lowercased().contains(searchText.lowercased())
+            })
+        }
+        self.collectionView?.reloadData()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        searchBar.isHidden = false
+    }
+    
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return photos?.count ?? 0
+//        return photos?.count ?? 0
+        return filterPhotos.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! HomeCell
-        cell.photo = self.photos![indexPath.item]
+//        cell.photo = self.photos![indexPath.item]
+        cell.photo = self.filterPhotos[indexPath.item]
         return cell
     }
     
@@ -78,6 +111,7 @@ class MainController: UICollectionViewController, UICollectionViewDelegateFlowLa
     }
     
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        searchBar.isHidden = true
         let photoDetailController = PhotoDetailController()
         photoDetailController.photo = photos![indexPath.item]
         navigationController?.pushViewController(photoDetailController, animated: true)
